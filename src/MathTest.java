d01837c61158013d3a075e5826d34a57c774272c
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2020-09-16 03:00:16 -0500

    Agregando clase MathTest clases predefinidas por java

---
+++ src/MathTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+public class MathTest {
+    
+    public static void main(String args[]){
+        System.out.println("15 de setiembre");
+        System.out.println("");
+        
+        //double a=4;
+        //int raiz=(int)Math.sqrt(a);
+        float num1=5.85F;
+        int resultado=Math.round(num1);
+        
+        System.out.println(resultado);
+        
+        
+        double num2=25.756;
+        int resultado1=(int)Math.round(num2);
+        System.out.println("");
+        System.out.println(resultado1);
+        double b=5,e=3;
+        int resultado2=(int)Math.pow(b, e);
+        System.out.println("");
+        System.out.println(resultado2);
+        
+        
+    }
+    
+    
+    
+    
+    
+}
